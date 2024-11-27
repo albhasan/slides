641b5a878c63f66ec16bc9dc70e21d6a4c846fbc
albhasan <albhasan@gmail.com>
2024-11-27 10:51:41 -0300

    Script updated to crop both P&t and dsig.

--- seasonmetrics/slides/scripts/crop_pthres_using_carvalho2021.R
+++ seasonmetrics/slides/scripts/crop_pthres_using_carvalho2021.R
@@ -7,12 +7,14 @@ library(terra)
 
 car_file <- "/home/alber/Documents/data/amazonian_fire_calendar/dry_season/AmzBasin_DrySeason_Length.tif"
 pthres_file <- "/home/alber/Documents/results/r_packages/seasonmetrics/results_05/season_peak_thres.tif"
-
+dsig_file   <- "/home/alber/Documents/results/r_packages/seasonmetrics/results_05/season_dsig.tif"
+#dsig_file   <- "/home/alber/Documents/results/r_packages/seasonmetrics/results_05/dsig_value_len.tif"
 
 pthres_world <- "/home/alber/Documents/github/slides/seasonmetrics/slides/images/pthres_world.png"
 
 car_r <- terra::rast(car_file)
 pthres_r <- terra::rast(pthres_file)
+dsig_r <- terra::rast(dsig_file)
 
 car_ext <- terra::ext(car_r)
 car_ext <- terra::project(car_ext,
@@ -25,8 +27,16 @@ car_r <- terra::crop(x = car_r, y = car_ext)
 pthres_r <- terra::crop(x = pthres_r, y = car_r)
 pthres_r <- terra::resample(x = pthres_r, y = car_r, method = "near")
 
-res <- terra::mask(x = pthres_r, mask = car_r)
+dsig_r <- terra::crop(x = dsig_r, y = car_r)
+dsig_r <- terra::resample(x = dsig_r, y = car_r, method = "near")
+
+res_pthres <- terra::mask(x = pthres_r, mask = car_r)
+res_dsig <- terra::mask(x = dsig_r, mask = car_r)
 
 out_file <- tools::file_path_sans_ext(pthres_file)
 out_file <- paste0(out_file, "_crop.tif")
-terra::writeRaster(res, filename = out_file)
+terra::writeRaster(res_pthres, filename = out_file)
+
+out_file <- tools::file_path_sans_ext(dsig_file)
+out_file <- paste0(out_file, "_crop.tif")
+terra::writeRaster(res_dsig, filename = out_file)
